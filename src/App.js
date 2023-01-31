dfb7101a260041b6ebabd7c1724aaf589555132d
Sohyb Mohamed <sohaip165@gmail.com>
2023-01-31 21:45:46 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+function Greet({ name, onClick }) {
+  // function handleClick() {
+  //   alert(`Hello ${name}`);
+  // }
+  return <button onClick={onClick}>Click me</button>;
+}
+function App() {
+  function handleClick() {
+    alert(`Hello`);
+  }
+  return (
+    <div className="App">
+      <div>
+        <Greet name="John" onClick={handleClick} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
